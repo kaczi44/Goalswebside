7fbb89bdd1903a66718a72795a3f84256ea340a0
Patryk Kaczmarek <patrykmiechow@interia.pl>
2016-02-27 15:38:55 +0100

    Added product entity and dbcontext

---
+++ GoalsWebsite/GoalsWebsite/DbContexts/WebsiteContext.cs
@@ -0,0 +1,19 @@
+﻿using GoalsWebsite.Entities;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace GoalsWebsite.DbContexts
+{
+    public class WebsiteContext : DbContext
+    {
+        public DbSet<Product> Products { get; set; }
+
+        public WebsiteContext() : base("DefaultConnection")
+        {
+
+        }
+    }
+}
---
+++ GoalsWebsite/GoalsWebsite/Entities/Product.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace GoalsWebsite.Entities
+{
+    public class Product
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public string Supplier { get; set; }
+        public double Price { get; set; }
+    }
+}
