b666a6f2046f8afecdac26c3c609994362970cdc
Patryk Kaczmarek <patrykmiechow@interia.pl>
2016-03-03 15:12:18 +0100

    added personal detals entitie and dbcontext

---
+++ GoalsWebsite/GoalsWebsite/DbContexts/PersonalDetalsContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using GoalsWebsite.Entities;
+
+namespace GoalsWebsite.DbContexts
+{
+    public class PersonalDetalsContext : DbContext
+    {
+        public  DbSet<PersonalDetals> Detals  { get; set; }
+
+        public PersonalDetalsContext():base("DefaultConnection")
+        {
+            
+        }
+    }
+}
---
+++ GoalsWebsite/GoalsWebsite/Entities/PersonalDetals.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity.ModelConfiguration.Conventions;
+using System.Linq;
+using System.Web;
+
+namespace GoalsWebsite.Entities
+{
+    public class PersonalDetals
+    {
+       public Guid id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Adres { get; set; }
+    }
+}
